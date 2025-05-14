b18ffe29829cb63c1711d17ab64d155b78a24084
Marisa <marisa.malveiro1@gmail.com>
2025-05-14 10:35:07 +0100

    varias alterações

--- DB/queries.php
+++ DB/queries.php
@@ -8,22 +8,22 @@
 
         // Primeira tentativa (busca pelo código do artigo + stock de um armazém específico)
         $sql1 = "SELECT TOP(1) 
-                    b.codigo, 
-                    b.nome, 
-                    b.iva, 
-                    b.familia, 
-                    b.unvenda, 
-                    c.pvpciva, 
-                    c.pvpsiva, 
-                    b.imagem,
-                ISNULL(s.existencia, 0) as 'existencia', 
-                ISNULL(s.armazem,0) as 'armazem'
-                FROM wgcartigos b
-                LEFT JOIN wgccodbarras a  ON a.artigo = b.codigo
-                LEFT JOIN wgcartigoslinhasprecos c ON b.codigo = c.artigo
-                LEFT JOIN wgcartarmazens s ON b.codigo = s.artigo AND (s.armazem = :armazem OR s.armazem IS NULL)
-                WHERE b.codigo = :codigo 
-                AND c.linha = :linha";
+                    artigos.codigo, 
+                    artigos.nome, 
+                    artigos.iva, 
+                    artigos.familia, 
+                    artigos.unvenda, 
+                    linhasprecos.pvpciva, 
+                    linhasprecos.pvpsiva, 
+                    artigos.imagem,
+                ISNULL(armazens.existencia, 0) as 'existencia', 
+                ISNULL(armazens.armazem,0) as 'armazem'
+                FROM wgcartigos artigos
+                LEFT JOIN wgccodbarras codbarras  ON codbarras.artigo = artigos.codigo
+                LEFT JOIN wgcartigoslinhasprecos linhasprecos ON artigos.codigo = linhasprecos.artigo
+                LEFT JOIN wgcartarmazens armazens ON artigos.codigo = armazens.artigo AND (armazens.armazem = :armazem OR armazens.armazem IS NULL)
+                WHERE artigos.codigo = :codigo 
+                AND linhasprecos.linha = :linha";
 
         $stmt1 = $conn->prepare($sql1);
         $stmt1->execute([
@@ -36,22 +36,22 @@
         // Se não encontrou, tenta a segunda query (busca pelo código de barras + stock de um armazém específico)
         if (!$artigo) {
             $sql2 = "SELECT TOP(1) 
-                        b.codigo, 
-                        b.nome, 
-                        b.iva, 
-                        b.familia, 
-                        b.unvenda, 
-                        c.pvpciva, 
-                        c.pvpsiva, 
-                        b.imagem,
-                    ISNULL(s.existencia, 0) as 'existencia', 
-                    ISNULL(s.armazem,0) as 'armazem'
-                    FROM wgcartigos b
-                    LEFT JOIN wgccodbarras a  ON a.artigo = b.codigo
-                    LEFT JOIN wgcartigoslinhasprecos c ON b.codigo = c.artigo
-                    LEFT JOIN wgcartarmazens s ON b.codigo = s.artigo AND (s.armazem = :armazem OR s.armazem IS NULL)
-                    WHERE a.codbarras = :codigo 
-                    AND c.linha = :linha";
+                        artigos.codigo, 
+                        artigos.nome, 
+                        artigos.iva, 
+                        artigos.familia, 
+                        artigos.unvenda, 
+                        linhasprecos.pvpciva, 
+                        linhasprecos.pvpsiva, 
+                        artigos.imagem,
+                    ISNULL(armazens.existencia, 0) as 'existencia', 
+                    ISNULL(armazens.armazem,0) as 'armazem'
+                    FROM wgcartigos artigos
+                    LEFT JOIN wgccodbarras codbarras  ON codbarras.artigo = artigos.codigo
+                    LEFT JOIN wgcartigoslinhasprecos linhasprecos ON artigos.codigo = linhasprecos.artigo
+                    LEFT JOIN wgcartarmazens armazens ON artigos.codigo = armazens.artigo AND (armazens.armazem = :armazem OR armazens.armazem IS NULL)
+                    WHERE codbarras.codbarras = :codigo 
+                    AND linhasprecos.linha = :linha";
 
             $stmt2 = $conn->prepare($sql2);
             $stmt2->execute([
@@ -62,6 +62,23 @@
             $artigo = $stmt2->fetch(PDO::FETCH_ASSOC);
         }
 
-        return $artigo; // Retorna os dados do artigo (com estoque do armazém específico) ou null se não encontrar
+        return $artigo; // Retorna os dados do artigo (com stock do armazém específico) ou null se não encontrar
+    }
+
+    function getDescontoCliente($clienteId) {
+        global $conn;
+    
+        $sql = "SELECT 
+                    terceiros.NumeroCliente,
+                    terceiros.Nome,
+                    terceiros.Zona,
+                    escontos.DESC1 AS Desconto
+                FROM Wgcterceiros terceiros
+                JOIN Wgcdescontos escontos ON terceiros.Zona = escontos.Zona
+                WHERE terceiros.NumeroCliente = :clienteId";
+    
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([':clienteId' => $clienteId]);
+        return $stmt->fetch(PDO::FETCH_ASSOC); // Retorna false se não existir o cliente na tabela
     }
 ?>
--- mostrar_artigo.php
+++ mostrar_artigo.php
@@ -13,60 +13,63 @@ if (!$artigo) {
 
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalhes do Produto</title>
-    <link rel="stylesheet" href="product_detail.css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="wwidth=device-width, initial-scale=1.0">
+        <title>Detalhes do Produto</title>
+        <link rel="stylesheet" href="product_detail.css">
 
-    <script>
-        setTimeout(function() {
-            window.location.href = "index.html"; // Redireciona para index.html após 10 segundos
-        }, 10000);
-    </script>
-</head>
-<body>
-    <?php if ($artigo): ?>
-        <?php
-            $imagemFinal = $config['caminhoImagens'] . "default.png"; // Imagem padrão
+        <script>
+           /* setTimeout(function() {
+                window.location.href = "index.html"; // Redireciona para index.html após 10 segundos
+            }, 10000);*/
+        </script>
+    </head>
+    <body>
+        <?php if ($artigo): ?>
+            <?php
+                $imagemFinal = $config['caminhoImagens'] . "default.png"; // Imagem padrão
 
-            if (!empty($artigo['imagem'])) {
-                $caminhoImagem = $config['caminhoImagens'] . $artigo['imagem'];
-                if (file_exists($_SERVER['DOCUMENT_ROOT'] . $caminhoImagem)) {
-                    $imagemFinal = $caminhoImagem;
+                if (!empty($artigo['imagem'])) {
+                    $caminhoImagem = $config['caminhoImagens'] . $artigo['imagem'];
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $caminhoImagem)) {
+                        $imagemFinal = $caminhoImagem;
+                    }
                 }
-            }
-        ?>
+            ?>
 
-        <div class="container">
-            <div class="info">
-                <h2><?= htmlspecialchars($artigo['nome']) ?></h2>
-                <div class="family">
-                    <p><?= htmlspecialchars($artigo['familia']) ?></p>
-                </div>
-                <div class="stock">
-                    <p><?= intval($artigo['existencia']) ?></p>
-                </div>
-            </div>
-            <div class="image_pvp">
-                <div class="image">
-                    <img src="<?= htmlspecialchars($imagemFinal) ?>" alt="Imagem do artigo">
-                </div>
-                <div class="pvp">
-                    <div class="price_iva">
-                        <span class="price"> <?= number_format(floatval($artigo['pvpciva']), 2, '.', '') ?>€</span>
-                        <span class="iva"> C/IVA</span>
+            <div class="container">
+                <div class="info">
+                    <h2><?= htmlspecialchars($artigo['nome']) ?></h2>
+                    <div class="family">
+                        <p><?= htmlspecialchars($artigo['familia']) ?></p>
                     </div>
-                    <p class="unvenda"><?= htmlspecialchars($artigo['unvenda']) ?></p>
                 </div>
-                <div class="logo">
-                    <img src="<?= htmlspecialchars($config['caminhoImagens'] . "logo.svg") ?>" alt="Logo">
+                <div class="image_pvp">
+                    <div class="image">
+                        <img src="<?= htmlspecialchars($imagemFinal) ?>" alt="Imagem do artigo">
+                    </div>
+                    <div class="pvp">
+                        <div class="price_iva">
+                            <span class="price"> <?= number_format(floatval($artigo['pvpciva']), 2, '.', '') ?>€</span>
+                            <span class="iva"> C/IVA (<?= number_format(intval($artigo['iva']))?>%)</span>
+                        </div>
+                        <p class="unvenda"><?= htmlspecialchars($artigo['unvenda']) ?></p>
+                    </div>
+                    <div class="logo">
+                        <img src="<?= htmlspecialchars($config['caminhoImagens'] . "logo.svg") ?>" alt="Logo">
+                        <div class="stock">
+                            <p><?= intval($artigo['existencia']) ?></p>
+                        </div>
+                    </div>
+                    
                 </div>
+            
             </div>
-        </div>
 
-    <?php else: ?>
-        <p>Artigo não encontrado!</p>
-    <?php endif; ?>
-</body>
+            
+        <?php else: ?>
+            <p>Artigo não encontrado!</p>
+        <?php endif; ?>
+    </body>
 </html>
